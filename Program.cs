2a8dd0f55e3c5a51398980510afb8318b535354a
prajyotnayak12 <110683704+prajyotnayak12@users.noreply.github.com>
2024-03-28 22:03:25 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class MUL
     public static void Main()
     {
         Console.Write("ENTER THE NUMBER WHOSE MULTIPLICATIN TABLE U WANT:"); 
-        int n = Convert.ToInt32(Console.ReadLine());
+        int n = Convert.ToInt64(Console.ReadLine());
         if (n > 0 && n < 100)
         {
             for (int i = 1; i <= 20; ++i)
