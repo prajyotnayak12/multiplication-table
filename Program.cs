0e5fe689df5f395636bbadfa7b72c45298027997
prajyotnayak12 <110683704+prajyotnayak12@users.noreply.github.com>
2024-03-28 17:28:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+class MUL
+{
+    public static void Main()
+    {
+        Console.Write("ENTER THE NUMBER WHOSE MULTIPLICATIN TABLE U WANT:"); 
+        int n = Convert.ToInt32(Console.ReadLine());
+        if (n > 0 && n < 100)
+        {
+            for (int i = 1; i <= 20; ++i)
+            {
+                Console.Write(n + " * " + i + " = " + n * i );
+
+            }
+        }
+        else
+        {
+            Console.WriteLine("ERROR");
+        }
+        Console.ReadLine();
+    }
+}
+
+
+
